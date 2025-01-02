acf8c9488c4086599dc8183b1a5a7893d2eac392
Mufid-031 <mufidmaxsus17@gmail.com>
2025-01-02 21:23:30 +0700

    admin dashboard layout

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,65 @@
+"use client";
+
+import { useAdminSidebar } from "@/contexts/AdminSidebarContext";
+import { cn } from "@/lib/utils";
 
 
 export default function AdminPage() {
+  const { isOpen } = useAdminSidebar();
   return (
-    <main>tes</main>
+    <div className={cn(
+      "p-6 space-y-6 transition-all duration-300",
+      isOpen ? "ml-64" : "ml-16"
+    )}>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="w-full h-40 bg-white rounded-md"></div>
+        <div className="w-full h-40 bg-white rounded-md"></div>
+        <div className="w-full h-40 bg-white rounded-md"></div>
+        <div className="w-full h-40 bg-white rounded-md"></div>
+        <div className="w-full p-5 bg-white rounded-md flex flex-col gap-4">
+          <h3 className="font-bold text-lg">Recent Uploads</h3>
+          <ul className="flex flex-col gap-4">
+            <li className="flex gap-2">
+              <div className="w-20 h-10 bg-gray-300 rounded"></div>
+              <div>
+                <h3 className="font-semibold">Wallpaper 1</h3>
+                <p className="text-sm">Uploaded 2 hours ago</p>
+              </div>
+            </li>
+            <li className="flex gap-2">
+              <div className="w-20 h-10 bg-gray-300 rounded"></div>
+              <div>
+                <h3 className="font-semibold">Wallpaper 1</h3>
+                <p className="text-sm">Uploaded 2 hours ago</p>
+              </div>
+            </li>
+            <li className="flex gap-2">
+              <div className="w-20 h-10 bg-gray-300 rounded"></div>
+              <div>
+                <h3 className="font-semibold">Wallpaper 1</h3>
+                <p className="text-sm">Uploaded 2 hours ago</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="w-full p-5 bg-white rounded-md flex flex-col gap-4">
+        <h3 className="font-bold text-lg">Top Categories</h3>
+        <ul className="flex flex-col gap-4">
+          <li className="flex justify-between">
+            <h3 className="font-semibold">Nature</h3>
+            <p>1,234 wallpapers</p>
+          </li>
+          <li className="flex justify-between">
+            <h3 className="font-semibold">Nature</h3>
+            <p>1,234 wallpapers</p>
+          </li>
+          <li className="flex justify-between">
+            <h3 className="font-semibold">Nature</h3>
+            <p>1,234 wallpapers</p>
+          </li>
+        </ul>
+        </div>
+      </div>
+    </div>
   );
 }
