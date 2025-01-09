00b73185d733b52e2aaedc1c640a73d990836371
Mufid-031 <mufidmaxsus17@gmail.com>
2025-01-09 20:31:36 +0700

    page home test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,16 @@
-"use client";
-
-import { signIn, useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { NavBar } from "@/components/home/Navbar"
+import { FilterBar } from "@/components/home/FilterBar"
+import { ImageGrid } from "@/components/home/ImageGrid"
 
 export default function Home() {
-  const session = useSession();
-
-  useEffect(() => {
-    console.log(session);
-  }, []);
   return (
-    <main>
-      <nav className="flex items-center justify-between p-4 h-20">
-        <h3 className="text-3xl font-semibold">NextWalls</h3>
-        <button onClick={() => signIn()}>SignIn</button>
-      </nav>
-
-      <main className="container mx-auto px-4 py-8">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 auto-rows-auto gap-4">
-        </div>
+    <div className="flex min-h-screen flex-col">
+      <NavBar />
+      <FilterBar />
+      <main className="flex-1">
+        <ImageGrid />
       </main>
-    </main>
-  );
+    </div>
+  )
 }
+
