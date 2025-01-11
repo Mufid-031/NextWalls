aaea950222a87bc818f998dcee96b70b67d5b595
Mufid-031 <mufidmaxsus17@gmail.com>
2025-01-11 17:40:48 +0700

    menampilkan wallpaper berdasarkan id yang dipilih

--- src/app/wallpapers/[id]/page.tsx
+++ src/app/wallpapers/[id]/page.tsx
@@ -4,6 +4,7 @@ import { NavBar } from "@/components/home/Navbar";
 import { useWallpaper } from "@/contexts/WallpaperContext";
 import { useIsomorphicLayoutEffect } from "@/hooks/useIsomorphicLayoutEffect";
 import { Wallpaper } from "@/types/wallpaper.type";
+import Image from "next/image";
 import React, { use, useState } from "react";
 
 export default function WallpaperIdPage({ params }: { params: Promise<{ id: string }> }) {
@@ -20,8 +21,13 @@ export default function WallpaperIdPage({ params }: { params: Promise<{ id: stri
       <header className="fixed top-0 z-30 w-full">
         <NavBar />
       </header>
-      <main className="flex-1 container px-10 pt-36">
-        <h1 className="text-4xl text-black">{wallpaper?.title}</h1>
+      <main className="flex-1 container bg-gray-950 grid grid-cols-[3fr_10fr]">
+        <div className="bg-gray-900">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="p-20">{wallpaper && <Image className="w-full h-full object-cover" unoptimized={true} src={wallpaper?.imageUrl} alt={wallpaper?.title} width={500} height={500} />}</div>
       </main>
     </div>
   );
